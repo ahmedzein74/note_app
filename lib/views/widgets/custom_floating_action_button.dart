17fa3c710a3e76448ec7ce070bfff01be821a986
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-08-02 17:36:01 +0300

    show Model Bottom Sheet

--- lib/views/widgets/custom_floating_action_button.dart
+++ lib/views/widgets/custom_floating_action_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/add_note_bottom_sheet.dart';
 
 class CustomFAB extends StatelessWidget {
   const CustomFAB({super.key});
@@ -9,7 +10,14 @@ class CustomFAB extends StatelessWidget {
       height: 70,
       width: 70,
       child: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return const AddNoteBottomSheet();
+            },
+          );
+        },
         backgroundColor: const Color.fromARGB(255, 16, 170, 160),
         shape: const CircleBorder(),
         child: const Icon(Icons.add),
